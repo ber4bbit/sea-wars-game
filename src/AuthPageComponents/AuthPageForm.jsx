bc503cb8bfc6e7cd20ea450af6e36c81a03d2dcb
jabka <ber4bbit@gmail.com>
2022-01-11 18:57:01 +0700

    Added buttons SignIn SignUp

--- src/AuthPageComponents/AuthPageForm.jsx
+++ src/AuthPageComponents/AuthPageForm.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-import './AuthPageForm.css'
+import './AuthPageForm.css';
 
 export default function AuthPageForm() {
     return (
-        <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
-            <article className="formContainer">
-                <TextField variant="outlined" label="Your email"/>
-                <TextField variant="outlined" label="Your password"/>
-            </article>
+        <div style={{ display: "flex", justifyContent: "center"}}>
+                <form action="" className="formContainer">
+                    <section style={{width: "100%", display: "flex", justifyContent: "space-between"}}>
+                        <TextField
+                            style={{width: "49%"}}
+                            variant="outlined"
+                            label="Your nickname *"
+                            margin="dense"
+                            color="success"
+                        />
+                        <TextField
+                            style={{width: "49%"}}
+                            variant="outlined"
+                            label="Your email *"
+                            margin="dense"
+                            color="success"
+                        />
+                    </section>
+                    <TextField
+                        variant="outlined"
+                        label="Your password *"
+                        margin="dense"
+                        color="success"
+                    />
+                    <Button variant="contained" color="success" style={{marginTop: "12px"}}>Sign In</Button>
+                    <Button variant="outlined" color="success" style={{marginTop: "12px"}}>Sign Up</Button>
+                </form>
         </div>
     )
 }
