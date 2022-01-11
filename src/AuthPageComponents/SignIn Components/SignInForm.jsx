5689619aa492362b80dc87440f286c38c25e372d
jabka <ber4bbit@gmail.com>
2022-01-12 02:53:49 +0700

    Added SignIn and SignUp form components + added modal window for SignUp form

---
+++ src/AuthPageComponents/SignIn Components/SignInForm.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import ModalWindow from "../ModalWindow/ModalWindow";
+import SignUpForm from "../SighUp Components/SignUpForm";
+
+export default function SignInForm() {
+    const [ modalActive, setModalActive ] = useState(false);
+    return(
+        <div style={{ display: "flex", justifyContent: "center"}}>
+            <form action="" className="formContainerSignIn">
+                <TextField
+                    variant="outlined"
+                    color="success"
+                    label="Your email *"
+                    margin="dense"
+                    style={{color: "white"}}
+                />
+                <TextField
+                    variant="outlined"
+                    color="success"
+                    label="Your password *"
+                    margin="dense"
+                />
+                <Button variant="contained" color="success" style={{marginTop: "12px"}}>Sign In</Button>
+                <h5>Or if you have no account yet create it</h5>
+                <Button variant="outlined" color="success" onClick={() => setModalActive(true)}>Sign Up</Button>
+                <ModalWindow active={ modalActive } setActive={ setModalActive }>
+                    <SignUpForm/>
+                </ModalWindow>
+            </form>
+        </div>
+    )
+}
